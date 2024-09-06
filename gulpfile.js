48f71853db233fd6e519008ba8c824c2e1c42daa
Furiak Anjelika <LikaF1@yandex.ru>
2024-09-06 23:31:03 +0300

    доббавлена обраотка файлов шрифтов и скрипта

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,19 @@ const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const mediaquery = require('postcss-combine-media-query'); 
 const htmlMinify = require('html-minifier');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
 
 function serve() {
   browserSync.init({
     server: {
       baseDir: './dist'
+    }
+  }, function(err, bs) {
+    if (err) {
+      console.log('BrowserSync encountered an error:', err);
+    } else {
+      console.log('BrowserSync is running');
     }
   });
 }
@@ -37,7 +45,14 @@ function html() {
                 .pipe(gulp.dest('dist/'))
         .pipe(browserSync.reload({stream: true}));
 } 
-
+function scripts() {
+  return gulp.src('src/scripts/*.js')
+    .pipe(plumber())
+    .pipe(uglify()) 
+    .pipe(gulp.dest('dist/scripts'))
+    .pipe(browserSync.reload({stream: true}))
+    .on('end', () => console.log('Scripts task completed'));
+}
 function css() {
     const plugins = [autoprefixer(),
         mediaquery()
@@ -50,28 +65,50 @@ function css() {
         .pipe(browserSync.reload({stream: true}));
 }
 
+
+const fs = require('fs');
+
 function images() {
   return gulp.src('src/images/*.{jpg,png,svg,gif,ico,webp,avif}')
     .pipe(gulp.dest('dist/images'))
-    .pipe(browserSync.reload({stream: true}));
+    .pipe(browserSync.reload({stream: true}))
+    .on('end', function() {
+      console.log('Images copied to dist/images');
+      // Проверяем, что файлы действительно существуют в папке dist/images
+      fs.readdir('dist/images', (err, files) => {
+        if (err) {
+          console.error('Error reading dist/images directory:', err);
+          return;
+        }
+        console.log('Files in dist/images:', files);
+      });
+    });
 }
-
+function fonts() {
+  return gulp.src('src/fonts/*')
+    .pipe(gulp.dest('dist/fonts'))
+    .pipe(browserSync.reload({stream: true}))
+    .on('end', () => console.log('Fonts copied to dist/fonts'));
+}
 function clean() {
-  return del('dist');
+  return del('dist').then(() => console.log('dist directory cleaned'));
 }
 
 function watchFiles() {
   gulp.watch(['src/*.html'], html);
   gulp.watch(['src/styles/*.css'], css);
-  gulp.watch(['src/images/*.{jpg,png,svg,gif,ico,webp,avif}'], images);
+  gulp.watch(['src/images/*.{jpg,png,svg,gif,ico,webp,avif}'], images).on('change', path => console.log(`File ${path} changed, running images task`));
+  gulp.watch(['src/scripts/*.js'], scripts).on('change', path => console.log(`File ${path} changed, running scripts task`));
+  gulp.watch(['src/fonts/*'], fonts);
 }
 
-const build = gulp.series(clean, gulp.parallel(html, css, images));
+const build = gulp.series(clean, gulp.parallel(html, css, images, scripts));
 const watchapp = gulp.parallel(build, watchFiles, serve);
 
 exports.html = html;
 exports.css = css;
 exports.images = images;
+exports.scripts = scripts;
 exports.clean = clean;
 
 exports.build = build;
